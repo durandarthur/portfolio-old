2bb487d4de189ee70bc1ad951360e79ffc6875db
arthurdurand <durandarthur09@outlook.com>
2022-11-10 21:13:33 +0000

    responsive design pt.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./gui.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      'gray': {
+        100: '#F2F2F2',
+        200: '#D7D7D7',
+        300: '#7F7F7F',
+        400: '#333333',
+        500: '#111111',
+      },
+    },
+    fontFamily: {
+      sans: ['Aileron', 'sans-serif'],
+      mono: ['RobotoMono', 'mono'],
+    },
+    extend: {
+      screens: {
+        '3xl': '1756px',
+      },
+    },
   },
   plugins: [],
 };
