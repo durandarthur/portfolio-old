81d9ed7e9908775d2eb623535c131195ee9b780b
arthurdurand <durandarthur09@outlook.com>
2022-11-12 12:03:10 +0000

    .

--- src/gui.tsx
+++ src/gui.tsx
@@ -1,61 +1,10 @@
 import { Component } from "react";
 import { Chrono } from "react-chrono";
+import { entries, entriesTitles, projects } from "./lib/data";
 
 export class Gui extends Component {
   render() {
-    const entries = [
-      {
-        title: "Décembre 2022\nAvril 2024",
-        cardTitle: "Concepteur Développeur d'Application (RNCP n°6)",
-        cardSubtitle: "O'clock, campus en ligne",
-        cardDetailedText:
-          "Formation de développeur web en alternance (1 semaine en formation / 2 semaines en entreprise)\nCompétences apprises: PHP, Node, React, conteneurs, unit testing, design patterns, SoC, sécurité, applications bureau, DevOps",
-      },
-      {
-        title: "2021-2022",
-        cardTitle: "Projet personnel",
-        cardSubtitle: "Année de césure",
-        cardDetailedText:
-          "Développement d'un site web avec React et Next.js, lié à une base de données postgresql avec fonctions CRUD.\nPermet de publier des idées de projet et d'en rechercher d'une façon similaire à un forum.",
-      },
-      {
-        title: "2021",
-        cardTitle: "Développeur fullstack",
-        cardSubtitle: "Creapli, Auzeville-Tolosane",
-        cardDetailedText:
-          "J'ai développé une application web (soumise à une clause de confidentalité) en C# et JavaScript utilisée dans la gestion de finances pour une chaîne de bijouteries.",
-      },
-      {
-        title: "2019-2021",
-        cardTitle: "Bac+2 Spécialité informatique",
-        cardSubtitle: "CESI Ecole D'Ingénieurs, Labège",
-        cardDetailedText:
-          "Formation C/C++, SQL, PHP, structures de données, algorithmes de tri/performance algorithmes",
-      },
-      {
-        title: "2017-2019",
-        cardTitle: "Baccalauréat STi2D",
-        cardSubtitle: "Cité Scolaire de Mirepoix",
-        cardDetailedText: "Spécialisation informatique (Systèmes d'Information et Numérique)",
-      },
-    ];
-    const entriesTitles = [
-      {
-        title: `Décembre 2022\nAvril 2024`,
-      },
-      {
-        title: "2021-2022",
-      },
-      {
-        title: "2021",
-      },
-      {
-        title: "2019-2021",
-      },
-      {
-        title: "2017-2019",
-      },
-    ];
+    
     // let ex: Array<object> = []; 
     // entries.map(entry => {
     //     let temp = {} as {title: string};
@@ -194,6 +143,14 @@ export class Gui extends Component {
               ))}
             </Chrono>
           </div>
+          <div className="flex h-[20vh] w-full space-x-4 mx-4">
+            {projects.map((project, i) => (
+            <div key={i} className="flex-1 bg-gray-400 rounded-xl">
+              <a href={project.link} className="text-2xl text-gray-200">{project.title}</a>
+            </div>
+          ))}
+          </div>
+          
         </main>
       </div>
     );
---
+++ src/lib/data.ts
@@ -0,0 +1,69 @@
+export const entries = [
+  {
+    title: "Décembre 2022\nAvril 2024",
+    cardTitle: "Concepteur Développeur d'Application (RNCP n°6)",
+    cardSubtitle: "O'clock, campus en ligne",
+    cardDetailedText:
+      "Formation de développeur web en alternance (1 semaine en formation / 2 semaines en entreprise)\nCompétences apprises: PHP, Node, React, conteneurs, unit testing, design patterns, SoC, sécurité, applications bureau, DevOps",
+  },
+  {
+    title: "2021-2022",
+    cardTitle: "Projet personnel",
+    cardSubtitle: "Année de césure",
+    cardDetailedText:
+      "Développement d'un site web avec React et Next.js, lié à une base de données postgresql avec fonctions CRUD.\nPermet de publier des idées de projet et d'en rechercher d'une façon similaire à un forum.",
+  },
+  {
+    title: "2021",
+    cardTitle: "Développeur fullstack",
+    cardSubtitle: "Creapli, Auzeville-Tolosane",
+    cardDetailedText:
+      "J'ai développé une application web (soumise à une clause de confidentalité) en C# et JavaScript utilisée dans la gestion de finances pour une chaîne de bijouteries.",
+  },
+  {
+    title: "2019-2021",
+    cardTitle: "Bac+2 Spécialité informatique",
+    cardSubtitle: "CESI Ecole D'Ingénieurs, Labège",
+    cardDetailedText:
+      "Formation C/C++, SQL, PHP, structures de données, algorithmes de tri/performance algorithmes",
+  },
+  {
+    title: "2017-2019",
+    cardTitle: "Baccalauréat STi2D",
+    cardSubtitle: "Cité Scolaire de Mirepoix",
+    cardDetailedText: "Spécialisation informatique (Systèmes d'Information et Numérique)",
+  },
+];
+
+export const entriesTitles = [
+  {
+    title: `Décembre 2022\nAvril 2024`,
+  },
+  {
+    title: "2021-2022",
+  },
+  {
+    title: "2021",
+  },
+  {
+    title: "2019-2021",
+  },
+  {
+    title: "2017-2019",
+  },
+];
+
+export const projects = [
+    {
+        title: "Portfolio website",
+        link: "/",
+    },
+    {
+        title: "Projectideas website",
+        link: "/",
+    },
+    {
+        title: "?",
+        link: "/",
+    },
+]
